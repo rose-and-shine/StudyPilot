410e277acdca861fd82fc8b012801a97a188671e
roselin-maria <roselin.mitmpl2023@learner.manipal.edu>
2026-09-01 18:49:49 +0530

    prepared for deploying backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 
 bootstrap();
